d976d450cddc2ceeea8062993f1bb33281640e70
Ryan Turner <c15ryan.turner@usafa.edu>
2013-12-09 14:47:18 -0700

    Initial commit

---
+++ IRsensor.c
@@ -0,0 +1,43 @@
+/*
+ * IRsensor.c
+ *
+ *  Created on: Dec 5, 2013
+ *      Author: C15Ryan.Turner
+ */
+#include <msp430g2553.h>
+#include "IRsensor.h"
+
+unsigned int getLeft() {
+	ADC10CTL0 &= ~ENC;
+	ADC10CTL1 &= ~(INCH3|INCH2|INCH1|INCH0);
+	ADC10CTL1 |= INCH_3;
+	ADC10CTL0 |= ENC + ADC10SC;
+	__bis_SR_register(CPUOFF + GIE);
+
+	return ADC10MEM;
+}
+
+/*unsigned int getCenter() {
+	ADC10CTL0 &= ~ENC;
+	ADC10CTL1 |= INCH_5;
+	ADC10CTL0 |= ENC + ADC10SC;
+	__bis_SR_register(CPUOFF + GIE);
+
+	return ADC10MEM;
+}*/
+
+unsigned int getRight() {
+	ADC10CTL0 &= ~ENC;
+	ADC10CTL1 &= ~(INCH3|INCH2|INCH1|INCH0);
+	ADC10CTL1 |= INCH_4;
+	ADC10CTL0 |= ENC + ADC10SC;
+	__bis_SR_register(CPUOFF + GIE);
+
+	return ADC10MEM;
+}
+
+// ADC10 interrupt service routine
+#pragma vector=ADC10_VECTOR
+__interrupt void ADC10_ISR(void) {
+	__bic_SR_register_on_exit(CPUOFF);        // Clear CPUOFF bit from 0(SR)
+}
---
+++ IRsensor.h
@@ -0,0 +1,19 @@
+/*
+ * IRsensor.h
+ *
+ *  Created on: Dec 5, 2013
+ *      Author: C15Ryan.Turner
+ */
+
+#ifndef IRSENSOR_H_
+#define IRSENSOR_H_
+
+void initLEDs();
+
+void initADC10();
+
+unsigned int getLeft();
+
+unsigned int getRight();
+
+#endif /* IRSENSOR_H_ */
